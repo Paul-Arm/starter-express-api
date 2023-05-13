052c8eb76cc164fae7e0ad27db7d0d6edf82be20
Paul <paul.pa36@gmail.com>
2023-05-14 01:11:34 +0200

    db

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const express = require('express')
 const app = express()
+const CyclicDb = require("@cyclic.sh/dynamodb")
+const db = CyclicDb("busy-erin-sawfish-coatCyclicDB")
+
+const games = db.collection("games")
+
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
+})
+
+app.all('/set', (req, res) => {
+    console.log("Just got a request!")
+    games.set("1",{
+        Name:"test",
+        gens:4,
+        map: "test map"
+    })
+    res.send('all set !')
+})
+
+
+app.all('/all', async (req, res) => {
+    console.log("Just got a request!")
+    let item = await animals.get("1")
+    res.send(item)
 })
 app.listen(process.env.PORT || 3000)
